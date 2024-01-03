91134004eefea44721a35442c482817145b75845
Vipin Negi <vipin20823@gmail.com>
2024-01-03 13:45:51 +0530

    initial commit

---
+++ src/main/java/com/carpool/web/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.carpool.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
